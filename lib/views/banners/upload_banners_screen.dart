4823196eaa8abfeec9ad53bcc00a98d326d8fdbb
Walter Kahamba <walterpylson@live.com.pt>
2023-10-01 01:07:20 +0100

    Designing the banner screen:

--- lib/views/banners/upload_banners_screen.dart
+++ lib/views/banners/upload_banners_screen.dart
@@ -1,19 +1,82 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
-class UploadBannerScreen extends StatelessWidget {
+class UploadBannerScreen extends StatefulWidget {
   static const String routeName = '/upload_banner_screen';
   const UploadBannerScreen({super.key});
 
   @override
+  State<UploadBannerScreen> createState() => _UploadBannerScreenState();
+}
+
+class _UploadBannerScreenState extends State<UploadBannerScreen> {
+  dynamic _image;
+  pickImage() async {
+    FilePickerResult? result = await FilePicker.platform
+        .pickFiles(allowMultiple: false, type: FileType.image);
+
+    if (result != null) {
+      setState(() {
+        _image = result.files.first.bytes;
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Container(
-        alignment: Alignment.bottomLeft,
-        padding: const EdgeInsets.all(10),
-        child: const Text(
-          'Upload Banner Screen',
-          style: TextStyle(fontWeight: FontWeight.w700, fontSize: 36),
-        ),
+      child: Column(
+        children: [
+          Container(
+            alignment: Alignment.bottomLeft,
+            padding: const EdgeInsets.all(10),
+            child: const Text(
+              'Banners ',
+              style: TextStyle(fontWeight: FontWeight.w700, fontSize: 26),
+            ),
+          ),
+          Divider(
+            color: Colors.grey.shade400,
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(14.0),
+                child: Column(
+                  children: [
+                    Container(
+                      height: 140,
+                      width: 140,
+                      decoration: BoxDecoration(
+                        color: Colors.grey.shade400,
+                        border: Border.all(color: Colors.grey.shade400),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: const Center(
+                        child: Text('Banner'),
+                      ),
+                    ),
+                    const SizedBox(height: 5),
+                    ElevatedButton(
+                      onPressed: () {
+                        pickImage();
+                      },
+                      child: const Text(
+                        'Carregar imagem',
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text(
+                  'Salvar',
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
